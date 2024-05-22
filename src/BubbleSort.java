e7e42e46ea2fcf1265b088cf4cb768a101d3a3ef
ErAbhi388 <er.abhishekinblr@gmail.com>
2024-05-23 00:00:26 +0530

    Pushing core java project

---
+++ src/BubbleSort.java
@@ -0,0 +1,27 @@
+
+public class BubbleSort {
+
+    public static void display(int[] arr) {
+        for(int j=0; j<arr.length; j++) {
+            System.out.print(arr[j] + ", ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12,10,5,3,98,2};
+
+        for(int i = 0; i<arr.length-1; i++) {
+            for(int j=0; j < arr.length-1-i; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int temp=arr[j];
+                    arr[j]= arr[j+1];
+                    arr[j+1]=temp;
+
+                }
+            }
+        }
+
+        display(arr);
+    }
+
+}
