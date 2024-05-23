e4e556e51fc3e65b4ed1d1c37e7cbe8d8777c5c7
ErAbhi388 <er.abhishekinblr@gmail.com>
2024-05-23 18:48:00 +0530

    Add selection sort

---
+++ src/SelectionSort.java
@@ -0,0 +1,26 @@
+public class SelectionSort {
+
+    public static void display(int[] arr) {
+        for(int j=0; j<arr.length; j++) {
+            System.out.print(arr[j] + ", ");
+        }
+    }
+
+    public static void main(String args[]) {
+        int[] arr = {7,8,3,1,2};
+
+        for(int i=0; i < arr.length-1; i++) {
+            int smallest = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[smallest] > arr[j]) {
+                    smallest = j;
+                }
+            }
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+
+        display(arr);
+    }
+}
