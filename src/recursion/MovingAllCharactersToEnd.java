af180de4b67c97ba48bb6558d251eaf4906da636
ErAbhi388 <er.abhishekinblr@gmail.com>
2024-06-09 20:37:15 +0530

    Pushing mivingallcharacterstoend

---
+++ src/recursion/MovingAllCharactersToEnd.java
@@ -0,0 +1,24 @@
+package recursion;
+
+public class MovingAllCharactersToEnd {
+
+    static void moveCharater(String s, int i, char c, String newString) {
+        if(i==s.length()-1) {
+            System.out.println(newString);
+           return;
+        }
+        if(s.charAt(i)==c) {
+
+            moveCharater(s, i+1, c, newString);
+        } else {
+            newString+=s.charAt(i);
+            moveCharater(s,i+1,c,newString);
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "Abbhibhek";
+        moveCharater(s,0,'b',"");
+        System.out.println();
+    }
+}
