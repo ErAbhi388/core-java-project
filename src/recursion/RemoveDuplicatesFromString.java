bcbf44e986caff14e67f99818f024945fccfbb10
ErAbhi388 <er.abhishekinblr@gmail.com>
2024-06-09 22:04:14 +0530

    Depoying RemoveDuplicatesFromString

---
+++ src/recursion/RemoveDuplicatesFromString.java
@@ -0,0 +1,32 @@
+package recursion;
+
+/*
+The author of this program is Mr Abhishek Mishra
+Date : 9th June, 2024
+The purpose of this program is to remove all the duplicates in a string
+ */
+
+public class RemoveDuplicatesFromString {
+
+    //String is abbfgbbhcc
+    public static boolean[] map = new boolean[26];
+    static void removeDuplicates(String s, int indx, String newString) {
+        if(indx == s.length()) {
+            System.out.println(newString);
+            return;
+        }
+        char current_character = s.charAt(indx);
+        if (map[current_character-'a']) {
+            removeDuplicates(s, indx+1,newString);
+        } else {
+            newString += current_character;
+            map[current_character-'a'] = true;
+            removeDuplicates(s, indx+1, newString);
+        }
+
+    }
+    public static void main(String[] args) {
+        String s = "abbcc";
+        removeDuplicates(s, 0, "");
+    }
+}
